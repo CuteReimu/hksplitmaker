2cfe52bcb90f11325cb1e39f01d00ba977757ece
mayn <415551921@qq.com>
2021-11-19 00:57:39 +0800

    Initial commit

---
+++ splitsfile.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/xml"
+	"errors"
+	"github.com/lxn/walk"
+	"io/ioutil"
+)
+
+type xmlRun struct {
+	XMLName              xml.Name `xml:"Run"`
+	Version              string   `xml:"version,attr"`
+	GameIcon             string
+	GameName             string
+	CategoryName         string
+	Metadata             xmlMetadata
+	Offset               string
+	AttemptCount         int
+	AttemptHistory       string
+	Segments             xmlSegments
+	AutoSplitterSettings xmlAutoSplitterSettings
+}
+
+type xmlMetadata struct {
+	Run       xmlMetadataRun
+	Platform  xmlMetadataPlatform
+	Variables string
+}
+
+type xmlMetadataRun struct {
+	Id string `xml:"id,attr"`
+}
+
+type xmlMetadataPlatform struct {
+	UsesEmulator string `xml:"usesEmulator,attr"`
+}
+
+type xmlSegments struct {
+	Segment []*xmlSegment
+}
+
+type xmlSegment struct {
+	Name            string
+	Icon            string
+	SplitTimes      xmlSplitTimes
+	BestSegmentTime string
+	SegmentHistory  string
+}
+
+type xmlSplitTimes struct {
+	SplitTime []xmlSplitTime
+}
+
+type xmlSplitTime struct {
+	Name string `xml:"name,attr"`
+}
+
+type xmlAutoSplitterSettings struct {
+	Ordered          string
+	AutosplitEndRuns string
+	Splits           xmlSplits
+}
+
+type xmlSplits struct {
+	Split []string
+}
+
+func saveSplitsFile() {
+	if err := checkSplitsFile(); err != nil {
+		walk.MsgBox(mainWindow, "内部错误", err.Error(), walk.MsgBoxIconError)
+		return
+	}
+	dlg := new(walk.FileDialog)
+	dlg.Title = "保存Splits文件"
+	dlg.Filter = "Splits文件（*.lss）|*.lss"
+	if ok, err := dlg.ShowSave(mainWindow); err != nil {
+		walk.MsgBox(mainWindow, "内部错误", err.Error(), walk.MsgBoxIconError)
+		return
+	} else if ok {
+		run := &xmlRun{
+			Version:  "1.7.0",
+			GameName: "Hollow Knight",
+			Metadata: xmlMetadata{Platform: xmlMetadataPlatform{UsesEmulator: "False"}},
+			Offset:   "00:00:00",
+			AutoSplitterSettings: xmlAutoSplitterSettings{
+				Ordered:          "True",
+				AutosplitEndRuns: "False",
+				Splits:           xmlSplits{},
+			},
+		}
+		for _, line := range lines {
+			run.Segments.Segment = append(run.Segments.Segment, &xmlSegment{
+				Name:       line.name.Text(),
+				SplitTimes: xmlSplitTimes{SplitTime: []xmlSplitTime{{Name: "Personal Best"}}},
+			})
+			run.AutoSplitterSettings.Splits.Split = append(run.AutoSplitterSettings.Splits.Split, splitsDict[line.splitId.Text()].id)
+		}
+		run.Segments.Segment = append(run.Segments.Segment, &xmlSegment{
+			Name:       finalLine.name.Text(),
+			SplitTimes: xmlSplitTimes{SplitTime: []xmlSplitTime{{Name: "Personal Best"}}},
+		})
+		if !endTriggerCheckBox.Checked() {
+			run.AutoSplitterSettings.AutosplitEndRuns = "True"
+			run.AutoSplitterSettings.Splits.Split = append(run.AutoSplitterSettings.Splits.Split, splitsDict[finalLine.splitId.Text()].id)
+		}
+		buf, err := xml.MarshalIndent(run, "", "  ")
+		if err != nil {
+			walk.MsgBox(mainWindow, "错误", err.Error(), walk.MsgBoxIconError)
+			return
+		}
+		err = ioutil.WriteFile(dlg.FilePath, append([]byte(xml.Header), buf...), 0644)
+		if err != nil {
+			walk.MsgBox(mainWindow, "错误", err.Error(), walk.MsgBoxIconError)
+			return
+		}
+		walk.MsgBox(mainWindow, "成功", "保存成功", walk.MsgBoxIconInformation)
+	}
+}
+
+func checkSplitsFile() error {
+	for _, line := range lines {
+		text := line.splitId.Text()
+		if _, ok := splitsDict[text]; !ok {
+			return errors.New(`自动分割配置"` + text + `"不存在，请检查`)
+		}
+	}
+	if !endTriggerCheckBox.Checked() {
+		text := finalLine.splitId.Text()
+		if _, ok := splitsDict[text]; !ok {
+			return errors.New(`自动分割配置"` + text + `"不存在，请检查`)
+		}
+	}
+	return nil
+}
---
+++ translate.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lxn/walk"
+	"github.com/xuri/excelize/v2"
+	"os"
+	"strings"
+)
+
+var translateDict = make(map[string]string)
+
+func init() {
+	xlsx, err := excelize.OpenFile("translate.xlsx")
+	if err != nil {
+		walk.MsgBox(nil, "错误", err.Error(), walk.MsgBoxIconError)
+		return
+	}
+	rows, err := xlsx.GetRows("Sheet1")
+	if err != nil {
+		walk.MsgBox(nil, "错误", err.Error(), walk.MsgBoxIconError)
+		os.Exit(-1)
+	}
+	for i, row := range rows {
+		key, val := strings.TrimSpace(row[0]), strings.TrimSpace(row[1])
+		if len(key) == 0 || len(val) == 0 {
+			walk.MsgBox(nil, "警告", fmt.Sprintf("第%d行出现空数据", i+1), walk.MsgBoxIconWarning)
+		} else {
+			translateDict[row[0]] = row[1]
+		}
+	}
+}
+
+func translate(s string) string {
+	s2 := doTranslate(s)
+	if s2 != s {
+		return strings.ReplaceAll(s2, " ", "")
+	}
+	return s
+}
+func doTranslate(s string) string {
+	if val, ok := translateDict[s]; ok {
+		return val
+	}
+	arr := strings.Split(s, " ")
+	for n := len(arr) - 1; n >= 1; n-- {
+		for i := 0; i+n <= len(arr); i++ {
+			key := strings.Join(arr[i:i+n], " ")
+			if val, ok := translateDict[key]; ok {
+				return doTranslate(strings.ReplaceAll(s, key, val))
+			}
+		}
+	}
+	return s
+}
