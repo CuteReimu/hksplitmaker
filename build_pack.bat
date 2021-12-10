658ea84a868811a7a14893f4f8aba685ff67b400
FlyingLu <415551921@qq.com>
2021-12-10 17:25:29 +0800

    Update README.md

--- build_pack.bat
+++ build_pack.bat
@@ -4,4 +4,4 @@ set zipname=计时器生成器.zip
 if exist %zipname% (
     del /F %zipname%
 )
-C:\Progra~1\WinRAR\Rar.exe a -r %zipname% hk-split-maker\src\asset\categories hk-split-maker\src\asset\icons hk-split-maker\src\asset\splits.txt LICENSE README.md hksplitmaker.exe translate.xlsx
+C:\Progra~1\WinRAR\Rar.exe a -r %zipname% hk-split-maker\src\asset\categories hk-split-maker\src\asset\icons hk-split-maker\src\asset\splits.txt LICENSE README.md hksplitmaker.exe translate.xlsx .github\hksplitmaker.png
