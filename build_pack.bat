c68651bf4734b7affe2844ca25defd3e341510f5
奇葩の灵梦 <415551921@qq.com>
2022-01-01 18:14:18 +0800

    优化了打包脚本

--- build_pack.bat
+++ build_pack.bat
@@ -1,7 +1,8 @@
-@echo off
-go build -ldflags -H=windowsgui -o hksplitmaker.exe github.com/CuteReimu/hksplitmaker || exit
+@echo on
+go build -ldflags "-s -w -H=windowsgui" -o hksplitmaker.exe github.com/CuteReimu/hksplitmaker || exit
+upx -1 hksplitmaker.exe
 set zipname=计时器生成器.zip
 if exist %zipname% (
     del /F %zipname%
 )
-C:\Progra~1\WinRAR\Rar.exe a -r %zipname% hk-split-maker\src\asset\categories hk-split-maker\src\asset\icons hk-split-maker\src\asset\splits.txt LICENSE README.md hksplitmaker.exe translate.xlsx .github\hksplitmaker.png
+C:\Progra~1\WinRAR\Rar.exe a -r %zipname% hk-split-maker\src\asset\categories hk-split-maker\src\asset\icons hk-split-maker\src\asset\splits.txt LICENSE README.md hksplitmaker.exe translate.xlsx
