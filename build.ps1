4788c8a2841bd8129005c16f52dcf3fd94a39fe5
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-06-27 22:16:22 +0800

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/LiveSplit/LiveSplit.AutoSplitters/master/LiveSplit.AutoSplitters.xml -OutFile LiveSplit.AutoSplitters.xml -v
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/ShootMe/LiveSplit.HollowKnight/master/Components/LiveSplit.HollowKnight.dll -OutFile LiveSplit.HollowKnight.dll -v
-wails build -webview2 embed
+wails build
--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 curl -O https://raw.githubusercontent.com/LiveSplit/LiveSplit.AutoSplitters/master/LiveSplit.AutoSplitters.xml
 curl -O https://raw.githubusercontent.com/ShootMe/LiveSplit.HollowKnight/master/Components/LiveSplit.HollowKnight.dll
 wails build
-wails build -platform=windows/amd64 -webview2 embed
+wails build -platform=windows/amd64
